90646f7f13826be144b0d56ac6a937980618483c
Joshua Rodriguez <josh030596@gmail.com>
2019-09-18 19:51:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase) 
   if "I LOVE YOU GRANDMA!" == phrase
   "I LOVE YOU TOO PUMPKIN!"
-  elsif "HUH?! SPEAK UP, SONNY!"
-  "HI!"
+  elsif phrase.upcase != phrase 
+  "HUH?! SPEAK UP, SONNY!"
   elsif "NO, NOT SINCE 1938!"
   "WHAT?"
   else "WHAT DID YOU EAT TODAY?"
